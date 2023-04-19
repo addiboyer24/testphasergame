e6b70753f411391add78c734d0336374454eacde
addiboyer24 <addison.boyer@umontana.edu>
2023-04-19 16:17:33 -0600

    trying something else.

--- assignment-12/scripts/main.js
+++ assignment-12/scripts/main.js
@@ -105,11 +105,12 @@ function create() {
 
     this.anims.create({
         key: 'turn',
-        frames: [{
-            key: 'dude',
-            frame: 4
-        }],
-        frameRate: 20
+        frames: this.anims.generateFrameNumbers('dude', {
+            start: 4,
+            end: 4
+        }),
+        frameRate: 20,
+        repeat: -1
     });
 
     this.anims.create({
